a1ec2def4a393dd3030e728f03c3691c112004ad
Rahul Bhargava <rahulb@media.mit.edu>
2014-04-30 14:27:33 -0400

    wrapper results in josn

--- src/main/java/org/mediameter/extractor/ExtractTextServlet.java
+++ src/main/java/org/mediameter/extractor/ExtractTextServlet.java
@@ -1,8 +1,10 @@
 package org.mediameter.extractor;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,13 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
+
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
 public class ExtractTextServlet extends HttpServlet {
 
+    public static String VERSION = "0.1";
+    public static String STATUS_OK = "ok";
+    public static String STATUS_ERROR = "error";
+    
     private static final Logger logger = LoggerFactory.getLogger(ExtractTextServlet.class);
+    private static Gson gson = new Gson();
 
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
 
         logger.info("Text Parse Request from "+request.getRemoteAddr());
@@ -25,6 +35,8 @@ public class ExtractTextServlet extends HttpServlet {
         response.setContentType("text/plain;charset=UTF=8");
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
+        long startTime = System.currentTimeMillis();
+        HashMap results = new HashMap();
         String urlString = request.getParameter("url");
         logger.info("Request to parse "+urlString);
         if(urlString==null){
@@ -34,14 +46,28 @@ public class ExtractTextServlet extends HttpServlet {
                 URL url = new URL(urlString);
                 String text = ArticleExtractor.INSTANCE.getText(url);
                 logger.info(text);
-                response.getWriter().write(text);
                 logger.info("done");
+                HashMap info = new HashMap();
+                info.put("url",urlString);
+                info.put("text", text);
+                results.put("results",info);
+                results.put("status",STATUS_OK);
             } catch (BoilerpipeProcessingException e) {
                 logger.error(e.toString());
-                response.getWriter().write(e.toString());            
+                HashMap error = new HashMap();
+                error.put("type",e.getClass().getName());
+                error.put("message",e.getMessage());
+                results.put("error", error);
+                results.put("status",STATUS_ERROR);
             }
         }
-        response.getWriter().flush();
+        long endTime = System.currentTimeMillis();
+        long elapsedMillis = endTime - startTime;
+        results.put("milliseconds", elapsedMillis);
+        
+        String jsonResults = gson.toJson(results);
+        logger.debug(jsonResults);
+        response.getWriter().write(jsonResults);
     }
     
 }
