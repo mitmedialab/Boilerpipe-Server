66f7fee5203a87f1220da6602055345102e5350f
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-09 11:43:42 -0400

    added HTMLextractor from Daniel da Silva Souza, University of Brasilia (UnB), Brazil (fixes #1)

--- src/main/java/org/mediameter/extractor/ExtractTextServlet.java
+++ src/main/java/org/mediameter/extractor/ExtractTextServlet.java
@@ -18,7 +18,6 @@ import de.l3s.boilerpipe.document.TextDocument;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
 import de.l3s.boilerpipe.sax.HTMLDocument;
-import de.l3s.boilerpipe.sax.HTMLFetcher;
 
 public class ExtractTextServlet extends HttpServlet {
 
