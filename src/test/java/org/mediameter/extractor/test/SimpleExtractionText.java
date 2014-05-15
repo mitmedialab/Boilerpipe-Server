fc6c1afe7778629f910e645706bf0f1559cf84b3
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-15 10:57:06 -0400

    use our own HTMLExtractor in the example

--- src/test/java/org/mediameter/extractor/test/SimpleExtractionText.java
+++ src/test/java/org/mediameter/extractor/test/SimpleExtractionText.java
@@ -2,12 +2,19 @@ package org.mediameter.extractor.test;
 
 import java.net.URL;
 
+import org.mediameter.extractor.HTMLFetcher;
+
+import de.l3s.boilerpipe.document.TextDocument;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
+import de.l3s.boilerpipe.sax.HTMLDocument;
 
 public class SimpleExtractionText {
     public static void main(final String[] args) throws Exception {
-        final URL url = new URL("http://www.theguardian.com/cities/2014/apr/30/rio-favelas-world-cup-olympics-vision-future-criminal-eyesore");
-        String text = ArticleExtractor.INSTANCE.getText(url);
+        URL url = new URL("http://finance.yahoo.com/news/ukraine-russia-eu-may-meet-094508779.html");
+        HTMLDocument htmlDoc = HTMLFetcher.fetch(url);
+        TextDocument doc = new BoilerpipeSAXInput(htmlDoc.toInputSource()).getTextDocument();
+        String text = ArticleExtractor.INSTANCE.getText(doc);
         System.out.println(text);
     }
 }
